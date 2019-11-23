d28ecc4b2f6331628c30affa9ad9fea5369f8438
sidereal <jesus.elares@gmail.com>
2019-11-23 15:50:23 -0500

    updated url spacing

--- scrape.py
+++ scrape.py
@@ -14,8 +14,8 @@ r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data, 'lxml')
 stocks = soup.find_all(True, {'class':
-                                  ['simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc(white) ',
-                                   'simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc($altRowColor) ']
+                                  ['simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc(white)',
+                                   'simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc($altRowColor)']
                               })
 
 
@@ -23,14 +23,14 @@ while (stocks != []):
     for stock in stocks:
         symbol = stock.find(True, {'class':['Fw(600)']}).text
         name = stock.find(True, {'class':['Va(m) Ta(start) Px(10px) Fz(s)']}).text
-        pricecap = stock.find_all(True, {'class':['Trsdu(0.3s) ']})
+        pricecap = stock.find_all(True, {'class':['Trsdu(0.3s)']})
         price = pricecap[0].text
         changes = stock.find_all(True, {'class':['Trsdu(0.3s) Fw(600) C($dataRed)', 'Trsdu(0.3s) Fw(600) C($dataGreen)', 'Trsdu(0.3s) Fw(600)']})
         change = changes[0].text
         percent_change = changes[1].text
 
         try:
-            market_cap = pricecap[2].text
+            market_cap = pricecap[3].text
         except:
             market_cap = "N/A"
 
@@ -45,8 +45,8 @@ while (stocks != []):
     soup = BeautifulSoup(data, 'lxml')
     stocks = soup.find_all(True, {'class':
                                       [
-                                          'simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc(white) ',
-                                          'simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc($altRowColor) ']
+                                          'simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc(white)',
+                                          'simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc($altRowColor)']
                                   })
 
 csv_file.close()
